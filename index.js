6638740a08b310ff0521abbacac347a097593874
Elgun Asgar <elgunasgerov@gmail.com>
2022-12-15 00:36:29 -0500

    commit message

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const validator = require("validator");
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.post("/api/shorturl", async (req, res) => {
+  const { url } = req.body;
+
+  if (!validator.isURL(url)) {
+    return res.send({ error: "Invalid URL" });
+  }
+
+  const randomNumber = Math.floor(Math.random() * 10000);
+
+  app.get(`/app/shorturl/${randomNumber}`, (req, res) => {
+    res.redirect(url);
+  });
+
+  res.json({ original_url: url, short_url: randomNumber });
+});
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
